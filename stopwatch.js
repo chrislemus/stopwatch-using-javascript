b223c0f6a4ada9ae62bb99c3bb484be3cb9b1470
chrislemus <icrislemus@gmail.com>
2021-04-02 11:10:34 -0400

    .

--- stopwatch.js
+++ stopwatch.js
@@ -22,15 +22,20 @@ clearButton.addEventListener('click', () => {
 })
 
 function startStopwatch() {
+  //reset start time
   stopwatch.startTime = Date.now();
+  //run `setInterval()` and save id
   stopwatch.intervalId = setInterval(() => {
+    //calculate elapsed time
     const elapsedTime = Date.now() - stopwatch.startTime + stopwatch.elapsedTime
+    //calculate different time measurements based on elapsed time
     const milliseconds = parseInt((elapsedTime%1000)/10)
     const seconds = parseInt((elapsedTime/1000)%60)
     const minutes = parseInt((elapsedTime/(1000*60))%60)
     const hour = parseInt((elapsedTime/(1000*60*60))%24);
+    //display time
     displayTime(hour, minutes, seconds, milliseconds)
-  }, 10);
+  }, 100);
 }
 
 function displayTime(hour, minutes, seconds, milliseconds) {
